4a5aab1ea38a5ecf9b22225f6c5de59e5444a355
Daniel Torres <denial.torres@gmail.com>
2021-11-23 19:38:08 -0600

    adding new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+20.each_with_index do |_param, index|
+  User.create(email: "example_#{index}@gmail.com", first_name: "First", last_name: "Last")
+end
